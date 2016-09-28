bb96278ac1a218ce4c657e11de0df50f6a86618c
q963181875060 <279340843@qq.com>
2016-09-28 20:13:00 +0800

    setup v5 from amazon

--- wordfence-waf.php
+++ wordfence-waf.php
@@ -1,8 +1,8 @@
 <?php
 // Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
 
-if (file_exists('/yjdata/www/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php')) {
-	define("WFWAF_LOG_PATH", '/yjdata/www/wordpress/wp-content/wflogs/');
-	include_once '/yjdata/www/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php';
+if (file_exists('/usr/share/nginx/html/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/usr/share/nginx/html/wp-content/wflogs/');
+	include_once '/usr/share/nginx/html/wp-content/plugins/wordfence/waf/bootstrap.php';
 }
 ?>
